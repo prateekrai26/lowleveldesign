4605265dfe947469d556dd81da16422c02a69982
Prateek Rai <hi.prateekrai@gmail.com>
2024-09-01 23:48:20 +0530

    SplitWise lld

---
+++ src/main/java/com/prateek/lld/deliverymanagementsystem/DeliveryManagementSystemApp.java
@@ -0,0 +1,22 @@
+package com.prateek.lld.deliverymanagementsystem;
+
+/*
+  Tenant can book delivery order
+ */
+
+class User{
+
+}
+
+
+class Product{
+    String productId;
+}
+
+class DeliveryManagementSystem{
+
+}
+
+
+public class DeliveryManagementSystemApp {
+}
--- src/main/java/com/prateek/lld/splitwise/SplitWiseApp.java
+++ src/main/java/com/prateek/lld/splitwise/SplitWiseApp.java
@@ -5,15 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Data
 class User{
   String userID;
   String name;
   List<Group> groupList;
+  User(String userID , String name){
+      this.userID = userID;
+      this.name = name;
+      groupList = new ArrayList<>();
+  }
 }
 
 @AllArgsConstructor
+@Data
 class Expense{
   String expenseID;
   String name;
@@ -22,51 +29,22 @@ class Expense{
 @Data
 abstract class Split{
   double amount;
+  String payingUserId;
   Map<String , Double> splitAmountMap;
   abstract Map<String,Double> calculateSplitAmount();
-}
-
-interface SplitFactory{
-   Split getSplit();
-}
-
-class PercentageSplitFactory implements SplitFactory{
-
-  @Override
-  public PercentageSplit getSplit() {
-    return new PercentageSplit();
-  }
-}
-
-class EqualSplitFactory implements SplitFactory{
-
-  @Override
-  public EqualSplit getSplit() {
-    return new EqualSplit();
-  }
-}
-
-class SplitByExactAmountFactory implements SplitFactory{
-
-  @Override
-  public SplitByExactAmount getSplit() {
-    return new SplitByExactAmount();
+  Split(){
+      splitAmountMap = new HashMap<>();
   }
 }
 
 class PercentageSplit extends  Split{
    Map<String , Double> percentageMap;
-  PercentageSplit(){
-     percentageMap = new HashMap<>();
+   PercentageSplit(String payingUserId , double amount , Map<String , Double> percentageMap){
+     this.percentageMap = percentageMap;
+     this.amount = amount;
+     this.payingUserId =  payingUserId;
+       calculateSplitAmount();
    }
-   void add(String userID , Double percentage){
-     percentageMap.put(userID , percentage);
-   }
-  void add(List<String> userIds , List<Double> percentages){
-    for(int i=0;i<userIds.size();i++){
-      percentageMap.put(userIds.get(i) , percentages.get(i));
-    }
-  }
 
   @Override
   Map<String, Double> calculateSplitAmount() {
@@ -80,18 +58,18 @@ class PercentageSplit extends  Split{
 
 class SplitByExactAmount extends Split{
   Map<String , Double> exactAmountMap;
-   SplitByExactAmount(){
-    exactAmountMap = new HashMap<>();
+   SplitByExactAmount(String payingUserId ,double amount , Map<String, Double> exactAmountMap){
+    this.exactAmountMap = exactAmountMap;
+    this.amount = amount;
+       this.payingUserId =  payingUserId;
+       calculateSplitAmount();
   }
+
   void add(String userID , Double amount){
     exactAmountMap.put(userID , amount);
   }
- void add(List<String> userIds , List<Double> amounts){
-     for(int i=0;i<userIds.size();i++){
-      exactAmountMap.put(userIds.get(i) , amounts.get(i));
-      }
- }
   @Override
+
   Map<String, Double> calculateSplitAmount() {
      for(String userID : exactAmountMap.keySet()){
        splitAmountMap.put(userID , exactAmountMap.get(userID));
@@ -101,34 +79,53 @@ class SplitByExactAmount extends Split{
 }
 
 class EqualSplit extends Split{
-  List<User> users;
-  EqualSplit(){
-    users = new ArrayList<>();
+  Set<String> users;
+  EqualSplit(String payingUserId ,double amount , List<String> users){
+    this.users = new HashSet<>(users);
+    this.amount = amount;
+      this.payingUserId =  payingUserId;
+    calculateSplitAmount();
   }
   void add(String userID){
-    users.add(new User());
+    users.add(userID);
+  }
+
+  void remove(String userId){
+       users.remove(userId);
   }
 
   @Override
   Map<String, Double> calculateSplitAmount() {
-    for(User user : users){
-       splitAmountMap.put(user.userID , amount/users.size());
+    for(String userId : users){
+       splitAmountMap.put(userId , amount/users.size());
     }
     return splitAmountMap;
   }
 }
+
+@Data
 class Group {
   String groupId;
-  Set<User> users;
+  String name;
+  Set<String> users;
   Map<String, List<Balance>> userToAmountMap;
   Map<String, Expense> expenseMap;
-
+  Group(String groupId , String name){
+      this.groupId = groupId;
+      this.name = name;
+      users = new HashSet<>();
+      userToAmountMap = new HashMap<>();
+      expenseMap = new HashMap<>();
+  }
   void addExpense(Expense expense) {
-    expenseMap.put(expense.expenseID, expense);
+     expenseMap.put(expense.expenseID, expense);
   }
 
   void deleteExpense(Expense expense) {
     expenseMap.remove(expense.expenseID, expense);
+  }
+  void addUser(String userId) {
+      users.add(userId);
   }
 }
 
@@ -168,6 +165,35 @@ class UserManager{
   }
 }
 
+interface BalanceCalculator{
+      Map<String , List<Balance>> calculateBalance(List<Expense> expenses);
+}
+
+class DefaultBalanceCalculator implements BalanceCalculator{
+
+    @Override
+    public Map<String, List<Balance>> calculateBalance(List<Expense> expenses) {
+       AtomicReference<Double> totalExpense = new AtomicReference<>(0.0);
+        expenses.stream().forEach(x-> totalExpense.set(totalExpense.get() + x.split.amount));
+        Map<String ,Double> totalExpenseMap = new HashMap<>();
+        Map<String , Double> payingUserAmoutMap = new HashMap<>();
+        for(Expense expense : expenses){
+           Map<String , Double> map = expense.getSplit().getSplitAmountMap();
+           for(String userID : map.keySet()){
+               totalExpenseMap.put(userID , totalExpenseMap.getOrDefault(userID , 0.0) + map.get(userID));
+           }
+            payingUserAmoutMap.put(expense.getSplit().payingUserId,  payingUserAmoutMap.getOrDefault(expense.getSplit().payingUserId , 0.0) + expense.split.amount);
+        }
+        Map<String ,  Double > balanceMap = new HashMap<>();
+        for(String userId  : totalExpenseMap.keySet()){
+            Double expenseAmount = totalExpenseMap.get(userId);
+            Double amountPaid =  payingUserAmoutMap.getOrDefault(userId , 0.0);
+            Double diff =  amountPaid - expenseAmount;
+            balanceMap.put(userId , diff);
+        }
+        return null;
+    }
+}
 
 class GroupManager{
   static GroupManager groupManager = new GroupManager();
@@ -183,21 +209,101 @@ class GroupManager{
      groupMap.get(groupId).addExpense(expense);
   }
 
+  List<String> getUsersFromGroup(String groupId){
+      return new ArrayList<>(groupMap.get(groupId).getUsers());
+  }
+
   void deleteExpense(String groupId ,Expense expense){
     groupMap.get(groupId).deleteExpense(expense);
   }
 
 
+  static GroupManager getInstance(){
+      return groupManager;
+  }
+  void addUserToGroup(String groupId , String userID){
+      groupMap.get(groupId).addUser(userID);
+  }
+  void createGroup(String userId , String groupId ,String name){
 
+      groupMap.put(groupId , new Group(groupId , name));
+      groupMap.get(groupId).addUser(userId);
 
+  }
+
+   Map<String, Expense> getExpenses(String groupId) {
+      return groupMap.get(groupId).getExpenseMap();
+    }
 }
 
 
 class SplitWise{
 
+    UserManager  userManager = UserManager.getInstance();
+
+    GroupManager groupManager = GroupManager.getInstance();
+
+   static SplitWise splitWise = new SplitWise();
+
+    private SplitWise(){}
+
+    static SplitWise getInstance() {
+        return splitWise;
+    }
+
+    void createUser(User user){
+        userManager.addUser(user);
+    }
+
+    void createGroup(String userId , String groupId ,String groupName){
+        User user = userManager.getUser(userId);
+        if(user != null){
+             groupManager.createGroup(userId , groupId , groupName);
+        }
+    }
+    void addMemberToGroup(String userId , String groupId){
+        if(userManager.getUser(userId)!=null) {
+            groupManager.addUserToGroup(groupId, userId);
+        }
+    }
+    void addExpense(String groupId , Expense expense){
+        groupManager.addExpense(groupId , expense);
+    }
+
+    List<String> getUsersFromGroup(String groupId){
+        return groupManager.getUsersFromGroup(groupId);
+    }
+
+    Map<String , Expense> getExpenseMap(String groupId){
+        return groupManager.getExpenses(groupId);
+    }
 
 }
 public class SplitWiseApp {
 
+    public static void main(String[] args) {
+        SplitWise splitWise = SplitWise.getInstance();
+        splitWise.createUser(new User("user1" , "prateek"));
+        splitWise.createUser(new User("user2" , "surbhhi"));
+        BalanceCalculator balanceCalculator = new DefaultBalanceCalculator();
+        splitWise.createGroup("user1" , "group1" , "travel");
 
+        splitWise.addMemberToGroup("user2" , "group1");
+
+        System.out.println(splitWise.getUsersFromGroup("group1"));
+
+        splitWise.addExpense("group1" ,
+                new Expense("expense1" , "bill" , new PercentageSplit("user1" , 100.0 ,
+                Map.of("user1" , 50.0 , "user2" , 50.0
+        ))));
+
+        splitWise.addExpense("group1" ,
+                new Expense("expense2" , "bill" , new SplitByExactAmount("user2" , 250.0 ,
+                        Map.of("user1" , 150.0 , "user2" , 100.0
+                        ))));
+
+        System.out.println(splitWise.getExpenseMap("group1"));
+
+        balanceCalculator.calculateBalance(new ArrayList<>(splitWise.getExpenseMap("group1").values()));
+    }
 }
