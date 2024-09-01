b0cfd2a9ddcef202f6981fc29a11419ca209ea82
Prateek Rai <35877862+prateekrai26@users.noreply.github.com>
2024-09-01 19:58:06 +0530

    Adding splitwise  LLD

--- src/main/java/com/prateek/lld/splitwise/SplitWiseApp.java
+++ src/main/java/com/prateek/lld/splitwise/SplitWiseApp.java
@@ -1,12 +1,10 @@
 package com.prateek.lld.splitwise;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
 class User{
@@ -14,6 +12,8 @@ class User{
   String name;
   List<Group> groupList;
 }
+
+@AllArgsConstructor
 class Expense{
   String expenseID;
   String name;
@@ -26,14 +26,47 @@ abstract class Split{
   abstract Map<String,Double> calculateSplitAmount();
 }
 
-class Percentage extends  Split{
+interface SplitFactory{
+   Split getSplit();
+}
+
+class PercentageSplitFactory implements SplitFactory{
+
+  @Override
+  public PercentageSplit getSplit() {
+    return new PercentageSplit();
+  }
+}
+
+class EqualSplitFactory implements SplitFactory{
+
+  @Override
+  public EqualSplit getSplit() {
+    return new EqualSplit();
+  }
+}
+
+class SplitByExactAmountFactory implements SplitFactory{
+
+  @Override
+  public SplitByExactAmount getSplit() {
+    return new SplitByExactAmount();
+  }
+}
+
+class PercentageSplit extends  Split{
    Map<String , Double> percentageMap;
-   Percentage(){
+  PercentageSplit(){
      percentageMap = new HashMap<>();
    }
    void add(String userID , Double percentage){
      percentageMap.put(userID , percentage);
    }
+  void add(List<String> userIds , List<Double> percentages){
+    for(int i=0;i<userIds.size();i++){
+      percentageMap.put(userIds.get(i) , percentages.get(i));
+    }
+  }
 
   @Override
   Map<String, Double> calculateSplitAmount() {
@@ -43,20 +76,27 @@ class Percentage extends  Split{
     return splitAmountMap;
   }
 }
+
+
 class SplitByExactAmount extends Split{
   Map<String , Double> exactAmountMap;
-  SplitByExactAmount(){
+   SplitByExactAmount(){
     exactAmountMap = new HashMap<>();
   }
   void add(String userID , Double amount){
     exactAmountMap.put(userID , amount);
   }
-
+ void add(List<String> userIds , List<Double> amounts){
+     for(int i=0;i<userIds.size();i++){
+      exactAmountMap.put(userIds.get(i) , amounts.get(i));
+      }
+ }
   @Override
   Map<String, Double> calculateSplitAmount() {
      for(String userID : exactAmountMap.keySet()){
        splitAmountMap.put(userID , exactAmountMap.get(userID));
      }
+     return splitAmountMap;
   }
 }
 
@@ -72,37 +112,84 @@ class EqualSplit extends Split{
   @Override
   Map<String, Double> calculateSplitAmount() {
     for(User user : users){
-       splitAmountMap.put(user , amount/users.size())
+       splitAmountMap.put(user.userID , amount/users.size());
     }
     return splitAmountMap;
   }
 }
-
-enum SplitType{
-  PERCENTAGE , EQUAL
-}
-class Group{
+class Group {
   String groupId;
-  List<User> users;
-  Map<String , List<Amount>> userToAmountMap;
-  List<Expense> expenses;
+  Set<User> users;
+  Map<String, List<Balance>> userToAmountMap;
+  Map<String, Expense> expenseMap;
+
+  void addExpense(Expense expense) {
+    expenseMap.put(expense.expenseID, expense);
+  }
+
+  void deleteExpense(Expense expense) {
+    expenseMap.remove(expense.expenseID, expense);
+  }
 }
 
-class Amount{
+class Balance{
   String id;
   Double amount;
   String userId;
 }
-class Owe extends Amount{
+class Owe extends Balance{
    Double collectMoney(Double amount){
      return null;
    }
 }
 
-class Owes extends Amount{
+class Owes extends Balance{
    void sendMoney(Double amount){
-
    }
+}
+
+class UserManager{
+  static UserManager userManager = new UserManager();
+  Map<String , User> userMap;
+  private UserManager(){
+     userMap = new HashMap<>();
+  }
+
+  void addUser(User user){
+    userMap.put(user.userID , user);
+  }
+
+  User getUser(String userID){
+    return userMap.get(userID);
+  }
+
+  static UserManager getInstance(){
+    return userManager;
+  }
+}
+
+
+class GroupManager{
+  static GroupManager groupManager = new GroupManager();
+  Map<String , Group> groupMap;
+  private GroupManager(){
+    groupMap = new HashMap<>();
+  }
+  void addGroup(Group group){
+    groupMap.put(group.groupId , group);
+  }
+
+  void addExpense(String groupId ,Expense expense){
+     groupMap.get(groupId).addExpense(expense);
+  }
+
+  void deleteExpense(String groupId ,Expense expense){
+    groupMap.get(groupId).deleteExpense(expense);
+  }
+
+
+
+
 }
 
 
